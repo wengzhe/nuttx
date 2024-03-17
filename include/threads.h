937c69b825ffe04aa6d8dde55ce3d1ed19bf0794
Peter Bee <bijunda1@xiaomi.com>
2024-03-18 00:50:27 +0800

    fix missing header
    Signed-off-by: Peter Bee <bijunda1@xiaomi.com>

--- include/threads.h
+++ include/threads.h
@@ -26,6 +26,7 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+#include <nuttx/sched.h>
 
 #include <sys/types.h>
 #include <pthread.h>
